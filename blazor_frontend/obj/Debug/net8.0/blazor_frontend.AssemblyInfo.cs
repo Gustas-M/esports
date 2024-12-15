f50fa72a06a1ee3ace34e04a399f6191aa7120a0
weirdhumanbeing <gustas.miliukas@gmail.com>
2024-12-15 23:42:26 +0200

    minor fixes

--- blazor_frontend/obj/Debug/net8.0/blazor_frontend.AssemblyInfo.cs
+++ blazor_frontend/obj/Debug/net8.0/blazor_frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a8599813a102512ea7f884ca514fd941632f06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb272d8d519425643d848cd29135a1be1250af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
